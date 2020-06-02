c94f943c9840623f7a4670775392f71582eadf7d
Derek Turner <turner.derek.b@gmail.com>
2020-06-02 20:00:08 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -19,13 +19,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {:name => "",
-         :age => "",
-         :attitude => ""
+        {:name => "Steven",
+         :age => "30",
+         :attitude => "confused"
         },
-        {:name => "",
-         :age => "",
-         :attitude => ""}
+        {:name => "Nurse",
+         :age => "44",
+         :attitude => "worried"}
         ]
    }
 }
